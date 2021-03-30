153d75a447127b656e6ac2ceda4ffe24601f3804
anousoneFS <sone.freestyle7898@gmail.com>
2021-03-30 21:28:56 +0700

    .

--- lib/app.dart
+++ lib/app.dart
@@ -23,6 +23,9 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
       ),
       home: LogInScreen(),
+      routes: [
+        '/lek-lao',
+      ],
     );
   }
 }
